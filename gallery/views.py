1ee19852de1bd50704d1d74e3ec343cdbb740c63
Mehran J <mehhran@gmail.com>
2021-12-30 13:43:42 -0800

    sorting and organizing stuff

--- gallery/views.py
+++ gallery/views.py
@@ -7,25 +7,30 @@ class Home(View):
         # stuff
         return render(request, 'gallery/home.html', {})
 
-class Detail(View):
-    def get(self, request):
-        # stuff
-        return render(request, 'gallery/detail.html', {})
 
 class PriceEntry(View):
     def get(self, request):
         # stuff
         return render(request, 'gallery/price_entry.html', {})
 
-class ProductList(View):
-    def get(self, request):
-        # stuff
-        return render(request, 'gallery/product_list.html', {})
 
 class AddProduct(View):
     def get(self, request):
         # stuff
         return render(request, 'gallery/add_product.html', {})
+
+
+class ProductDetail(View):
+    def get(self, request):
+        # stuff
+        return render(request, 'gallery/product_detail.html', {})
+
+
+class ProductList(View):
+    def get(self, request):
+        # stuff
+        return render(request, 'gallery/product_list.html', {})
+
 
 class ProductGallery(View):
     def get(self, request):
