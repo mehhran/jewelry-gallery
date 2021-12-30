cdbe71cf028f7ce35afc298a191294ab69a683ce
Mehran J <mehhran@gmail.com>
2021-12-30 13:37:45 -0800

    pages

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from gallery.views import Home
+from gallery.views import Detail, Home, PriceEntry, ProductList, AddProduct, ProductGallery
 
 urlpatterns = [
     path("", Home.as_view(), name="gallery_home"),
+    path("detail", Detail.as_view(), name="detail_get"),
+    path("price_entry", PriceEntry.as_view(), name="price_entry"),
+    path("product_list", ProductList.as_view(), name="product_list"),
+    path("add_product", AddProduct.as_view(), name="add_product"),
+    path("product_gallery", ProductGallery.as_view(), name="product_gallery"),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -6,3 +6,28 @@ class Home(View):
     def get(self, request):
         # stuff
         return render(request, 'gallery/home.html', {})
+
+class Detail(View):
+    def get(self, request):
+        # stuff
+        return render(request, 'gallery/detail.html', {})
+
+class PriceEntry(View):
+    def get(self, request):
+        # stuff
+        return render(request, 'gallery/price_entry.html', {})
+
+class ProductList(View):
+    def get(self, request):
+        # stuff
+        return render(request, 'gallery/product_list.html', {})
+
+class AddProduct(View):
+    def get(self, request):
+        # stuff
+        return render(request, 'gallery/add_product.html', {})
+
+class ProductGallery(View):
+    def get(self, request):
+        # stuff
+        return render(request, 'gallery/product_gallery.html', {})
