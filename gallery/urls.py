a92b9cb89aae98913da0f15b3136e36dd0f55414
Mehran J <mehhran@gmail.com>
2022-01-03 16:48:51 -0800

    list, gallery and more

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path("", Home.as_view(), name="gallery_home"),
     path("price_entry", PriceEntry.as_view(), name="price_entry"),
     path("add_product", AddProduct.as_view(), name="add_product"),
-    path("product_detail", ProductDetail.as_view(), name="detail_get"),    
+    path("product_detail", ProductDetail.as_view(), name="detail_get"),
+    path("product_detail/<int:pid>/", ProductDetail.as_view(), name="detail_link"),    
     path("product_list", ProductList.as_view(), name="product_list"),    
     path("product_gallery", ProductGallery.as_view(), name="product_gallery"),
 ]
